5b94f6488265d88b007718122b2462b50692a7f3
yleprince <yrieix.leprince@gmail.com>
2020-05-20 00:00:06 +0200

    adding tests for the ravioli data structure

---
+++ tests/test_ravioli.py
@@ -0,0 +1,35 @@
+import os
+import unittest
+
+import pandas as pd
+
+from ravioli.datastructure import Ravioli
+
+
+class TestInitRavioli(unittest.TestCase):
+    """Checks that the ravioli class succeeds during init"""
+
+    def setUp(self):
+        self.path = os.path.dirname(__file__)
+        self.filepath = os.path.join(self.path, "samples.csv")
+
+    def test_init(self):
+        df = Ravioli(self.filepath)
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertIsInstance(df, Ravioli)
+        self.assertEqual(df.shape, (40, 11))
+
+    def test_init_args(self):
+        columns = [
+            "pickup_datetime",
+            "dropoff_datetime",
+            "pickup_longitude",
+            "pickup_latitude",
+            "dropoff_longitude",
+            "dropoff_latitude",
+            "trip_duration",
+        ]
+        df = Ravioli(self.filepath, nrows=26, usecols=columns)
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertIsInstance(df, Ravioli)
+        self.assertEqual(df.shape, (26, 7))
